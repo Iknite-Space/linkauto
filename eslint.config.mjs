f2936ae818ec77877f9a9a92e6b204b6360402ad
tonny <tonyarrey50@gmail.com>
2025-06-11 17:46:45 +0100

    added eslint, golint and added the eslint in the script part of the lock.json file

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+// eslint.config.mjs
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import parser from "@babel/eslint-parser";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser, 
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      js,
+      react: pluginReact,
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+]);
