a65697fdd48c629c308b639d1b2523e50b455193
Kedjuprecious <=>
2025-07-09 15:36:14 +0000

    Added the colors for the design

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -6,16 +6,26 @@
 export const theme = {
     extend: {
         colors: {
-            primary: "#0077cc", // Example primary color
-            secondary: "#ff5722", // Example secondary color
-            background: "#f0f0f0", // Example background color
-            textColor: "#333333", // Example text color
+            primary: "#01285b", // Royal blue
+            secondary: "#6C7A89", // Slate gray
+            accent: "#FFC107", // Amber yellow
+            backgroundColor: "#F8F9FA", // Background-color
+            cardBackground: "#FFFFFF", // Card background for containers tat have content
+            PrimaryTextColor: "#1C1C1C", // Eerie black, 
+            SecondaryTextColor: "#4F4F4F", // Dark gray
+            green: "#28A745", // Green for success messages
+            red: "#DC3545", // Red for error messages
         },
+
         fontFamily: {
-            sans: ["Arial", "sans-serif"],
-            serif: ["Georgia", "serif"],
-            mono: ["Courier New", "monospace"],
+            sans: ['Roboto', 'Arial', 'sans-serif'],
         },
+
+        fontSize: {
+            heading: ['24pt', { lineHeight: '1.3', fontWeight: '700' }], // Roboto Bold
+            body: ['14pt', { lineHeight: '1.6', fontWeight: '400' }],    // Roboto Regular
+            caption: ['12pt', { lineHeight: '1.4', fontWeight: '300' }], // Roboto Light or Regular
+       },
     },
 };
 export const plugins = [];
