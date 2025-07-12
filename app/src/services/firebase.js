ef83c318d9a0bdbaf0e46c69dbd5f3b5ad4faa4b
ichami630 <brandonichami@gmail.com>
2025-07-12 13:40:15 +0100

    load the env variables on frontend

--- app/src/services/firebase.js
+++ app/src/services/firebase.js
@@ -4,12 +4,12 @@ import {getAuth, createUserWithEmailAndPassword,sendEmailVerification} from "fir
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBJ_vipcWkelnSTwDG0RF7wfwinXWn5F3I",
-  authDomain: "autolink-883fc.firebaseapp.com",
-  projectId: "autolink-883fc",
-  storageBucket: "autolink-883fc.firebasestorage.app",
-  messagingSenderId: "573372789363",
-  appId: "1:573372789363:web:d7c1b750ec2f586f867006"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
