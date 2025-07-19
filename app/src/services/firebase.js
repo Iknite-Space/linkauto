4906c0e672d5cf5eaa7e960ff5908843016b6288
ichami630 <brandonichami@gmail.com>
2025-07-19 09:36:08 +0100

    pushing env to force test

--- app/src/services/firebase.js
+++ app/src/services/firebase.js
@@ -1,15 +1,23 @@
 
 import { initializeApp } from "firebase/app";
 import {getAuth, createUserWithEmailAndPassword,sendEmailVerification,signInWithEmailAndPassword, sendPasswordResetEmail, signOut} from "firebase/auth"
+import api from "./axios";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID
+
+  apiKey: "AIzaSyD-AIzaSyBJ_vipcWkelnSTwDG0RF7wfwinXWn5F3I-KEY",
+  authDomain: "autolink-883fc.firebaseapp.com",
+  projectId: "autolink-883fc",
+  storageBucket: "autolink-883fc.firebasestorage.app",
+  messagingSenderId: "573372789363",
+  appId: "1:573372789363:web:d7c1b750ec2f586f867006"
 };
 
 // Initialize Firebase
