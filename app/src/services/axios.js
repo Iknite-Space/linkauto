f220b94cf9c43ca2297ea50fc5bf9538aec9faa6
ichami630 <brandonichami@gmail.com>
2025-07-19 11:21:54 +0100

    fixing deployment

--- app/src/services/axios.js
+++ app/src/services/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: process.env.REACT_APP_AXIOS_BASE_URL,
-    baseURL: 'api.linkauto.xyz',
+    baseURL: 'https://api.linkauto.xyz',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
