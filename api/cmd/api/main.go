0e6482f5afb2a5c73880fc222dae5b8187f6f181
ichami630 <brandonichami@gmail.com>
2025-07-19 10:57:16 +0100

    testing frontend and backend online

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -144,9 +144,12 @@ func getPostgresConnectionURL(config DBConfig) string {
 // corsMiddleware adds CORS headers to responses
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Adjust this to your frontend's actual origin
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE,PATCH, OPTIONS")
+		origin := r.Header.Get("Origin")
+		if origin == "http://localhost:3000" || origin == "https://linkauto.xyz" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if r.Method == "OPTIONS" {
