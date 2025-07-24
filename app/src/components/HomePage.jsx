7bc209a24acfec79bdfb73bef603f6f5b4d49f27
tonny <tonyarrey50@gmail.com>
2025-07-24 19:20:39 +0100

    commiting changes

--- app/src/components/HomePage.jsx
+++ app/src/components/HomePage.jsx
@@ -1,26 +1,26 @@
-
-import React from 'react'
-import HeroSection from './HeroSection'
-import WhyChooseUs from './WhyChooseUs'
-import AboutUs from './AboutUs'
+import React from "react";
+import HeroSection from "./HeroSection";
+import WhyChooseUs from "./WhyChooseUs";
+import AboutUs from "./AboutUs";
 import Faq from "./Faq";
-
 
 const HomePage = () => {
   return (
     <>
-
+      <div id="hero-section">
+        <HeroSection />
+      </div>
+      <div id="about-us" className="my-4">
+        <AboutUs />
+      </div>
+      <div className="my-4">
+        <WhyChooseUs />
+      </div>
       <div className="my-4" id="FAQ">
         <Faq />
       </div>
     </>
   );
 };
-    <div id='hero-section'><HeroSection /></div>
-    <div id="about-us" className="my-4"><AboutUs /></div>
-    <div className="my-4"><WhyChooseUs /></div>
-    </>
-  )
-}
 
 export default HomePage;
