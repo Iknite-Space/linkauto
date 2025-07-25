2eca947eece07fb5eac793101e13a46b57eb40c9
Kedjuprecious <kedjuprecious@gmail.com>
2025-07-25 14:28:05 +0100

    Adding the endpoints

--- app/src/components/NavBar.jsx
+++ app/src/components/NavBar.jsx
@@ -29,21 +29,16 @@ const Navbar = () => {
         {/* Desktop Menu */}
         <div className="hidden md:flex items-center w-full">
           <div className="flex-1 flex justify-center space-x-6 text-heading2 text-backgroundColor">
-            <a href="#" className="hover:text-accent">Home</a>
-            <a href="#" className="hover:text-accent">About Us</a>
-            <a href="#" className="hover:text-accent">Why Choose Us</a>
-            <a href="#" className="hover:text-accent">Testimonials</a>
-            <a href="#" className="hover:text-accent">Contact</a>
-            <a href="#" className="hover:text-accent">FAQ</a>
+            <a href="#home" className="hover:text-accent">Home</a>
+            <a href="#about-us" className="hover:text-accent">About Us</a>
+            <a href="#why-choose-us" className="hover:text-accent">Why Choose Us</a>
+            <a href="#testimonials" className="hover:text-accent">Testimonials</a>
+            <a href="#contact" className="hover:text-accent">Contact</a>
+            <a href="#faq" className="hover:text-accent">FAQ</a>
           </div>
           <div className="flex items-center space-x-4">
             <Link to="/login" className="text-white hover:text-accent">Login</Link>
-            <a
-              href="#"
-              className="bg-accent text-white text-button px-4 py-2 rounded hover:bg-secondary shadow"
-            >
-              Register
-            </a>
+            <Link to="/register" className="bg-accent text-white text-button px-4 py-2 rounded hover:bg-secondary shadow">Register</Link>
           </div>
         </div>
 
